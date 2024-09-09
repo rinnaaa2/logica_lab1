e94fc4ca56a8561332f5706d00d365890ac87d58
Rina <azm2m@mail.ru>
2024-09-09 22:14:34 +0300

    задание 2

--- log_lab1/log_lab1.cpp
+++ log_lab1/log_lab1.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {
+	srand(time(0));
 	setlocale(LC_ALL, "");
-	int max = INT_MIN, min = INT_MAX, razn, mass[10] = {1, 45, 85, 94, 4, 36, 63, 22, 7, 85};
+	int max = INT_MIN, min = INT_MAX, razn, mass[10];
 
 	printf("Массив:");
 	for (int i = 0; i < 10; i++) {
+		mass[i] = rand() % 100;
 		printf("%3d", mass[i]);
 		if (mass[i] >= max) {
 			max = mass[i];
@@ -16,7 +18,6 @@ int main()
 		}
 	}
 	razn = max - min;
-	printf("\nМаксимальный элемент:%d\nМинимальный элемент:%d\nРазница между максимальным и минимальным:%d", max, min,razn);
+	printf("\nМаксимальный элемент:%d\nМинимальный элемент:%d\nРазница:%d", max, min, razn);
 	return 0;
 }
-
