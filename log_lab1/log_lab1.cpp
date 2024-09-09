6c267d5189bd580eafa6d4ea89e88802ce754efc
Rina <azm2m@mail.ru>
2024-09-10 00:47:29 +0300

    задание 5

--- log_lab1/log_lab1.cpp
+++ log_lab1/log_lab1.cpp
@@ -1,48 +1,81 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#define MAX_STUDENTS 100 // Максимальное количество студентов
 
-int main()
-{
-	srand(time(0));
-	setlocale(LC_ALL, "");
-	int stroca,stolb, max = INT_MIN, min = INT_MAX, razn;
-	printf("Введите кол-во строк:");
-	scanf("%d", &stroca);
-	printf("Введите кол-во столбцов:");
-	scanf("%d", &stolb);
-	
-	int **mass = (int**)malloc(stroca*sizeof(int*));
-	for (int i = 0; i < stroca; i++) {
-		mass[i] = (int*)malloc(stolb * sizeof(int));
-	}
+struct student {
+    char famil[50];
+    char name[50];
+    int nomzach;
+    char facult[10];
+};
 
-	printf("Массив:");
-	for (int i = 0; i < stroca; i++) {
-		printf("\n");
-		for (int j = 0; j < stolb; j++) {
-			mass[i][j] = rand() % 100;
-			printf("%3d", mass[i][j]);
-			if (mass[i][j] >= max) {
-				max = mass[i][j];
-			}
-			if (mass[i][j] <= min) {
-				min = mass[i][j];
-			}
-		}
-	}
+//поиска студента 
+struct student* search(struct student stud[], int num_students, char* famil, char* name, char* facult, int nomzach) {
+    for (int i = 0; i < num_students; i++) {
+        if (strcmp(stud[i].famil, famil) == 0 &&
+            strcmp(stud[i].name, name) == 0 &&
+            strcmp(stud[i].facult, facult) == 0 &&
+            stud[i].nomzach == nomzach) {
+            return &stud[i]; // Возвращаем указатель на найденного студента
+        }
+    }
+    return NULL; // Студент не найден
+}
 
-	printf("\nСумма по столбцам:\n");
-	for (int j = 0; j < stolb; j++) {
-		int sum = 0;
-		for (int i = 0; i < stroca; i++) {
-			sum += mass[i][j];
-		}
-		printf("Столбец %d: %d\n", j + 1, sum);
-	}
-		
-	razn = max - min;
-	printf("\nМаксимальный элемент:%d\nМинимальный элемент:%d\nРазница:%d", max, min, razn);
+int main() {
+    setlocale(LC_ALL, "");
+    // Создаем массив структур students
+    struct student stud[MAX_STUDENTS];
+    int num_students = 0;
 
-	free(mass);
-	return 0;
+    printf("Введите данные о студентах (введите '*', чтобы закончить):\n");
+    char input_famil[50], input_name[50], input_facult[10];
+    int input_nomzach;
+    while (1) {
+        printf("Фамилия: ");
+        scanf("%s", input_famil);
+        if (strcmp(input_famil, "*") == 0) {
+            break;
+        }
+        printf("Имя: ");
+        scanf("%s", input_name);
+        printf("Номер зачётки: ");
+        scanf("%d", &input_nomzach);
+        printf("Факультет: ");
+        scanf("%s", input_facult);
+
+        strcpy(stud[num_students].famil, input_famil);
+        strcpy(stud[num_students].name, input_name);
+        stud[num_students].nomzach = input_nomzach;
+        strcpy(stud[num_students].facult, input_facult);
+
+        num_students++;
+    }
+
+    char search_famil[50], search_name[50], search_facult[10];
+    int search_nomzach;
+    printf("\nВведите фамилию студента для поиска: ");
+    scanf("%s", search_famil);
+    printf("Введите имя студента для поиска: ");
+    scanf("%s", search_name);
+    printf("Введите номер зачётки студента для поиска: ");
+    scanf("%d", &search_nomzach);
+    printf("Введите факультет студента для поиска: ");
+    scanf("%s", search_facult);
+
+    struct student* search_student = search(stud, num_students, search_famil, search_name, search_facult, search_nomzach);
+
+    if (search_student != NULL) {
+        printf("Студент найден:\n");
+        printf("Фамилия: %s\n", search_student->famil);
+        printf("Имя: %s\n", search_student->name);
+        printf("зачётка: %d\n", search_student->nomzach);
+        printf("Факультет: %s\n", search_student->facult);
+    }
+    else {
+        printf("Студент не найден.\n");
+    }
+    return 0;
 }
