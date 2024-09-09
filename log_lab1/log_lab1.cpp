11d95fd7e720a7edc3cec8b167649c7f11ccc563
Rina <azm2m@mail.ru>
2024-09-09 22:28:23 +0300

    задание 3

--- log_lab1/log_lab1.cpp
+++ log_lab1/log_lab1.cpp
@@ -1,13 +1,19 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 
 int main()
 {
 	srand(time(0));
 	setlocale(LC_ALL, "");
-	int max = INT_MIN, min = INT_MAX, razn, mass[10];
+	int size=0, max = INT_MIN, min = INT_MAX, razn;
+	printf("Введите размер массива:");
+	scanf("%d", &size);
+
+	int* mass;
+	mass = (int*)malloc(size * sizeof(int));
 
 	printf("Массив:");
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < size; i++) {
 		mass[i] = rand() % 100;
 		printf("%3d", mass[i]);
 		if (mass[i] >= max) {
@@ -19,5 +25,7 @@ int main()
 	}
 	razn = max - min;
 	printf("\nМаксимальный элемент:%d\nМинимальный элемент:%d\nРазница:%d", max, min, razn);
+
+	free(mass);
 	return 0;
 }
