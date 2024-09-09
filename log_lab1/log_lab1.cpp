7c0bfbc06dfe8d57e146b1df81c81635ce17e9a9
Rina <azm2m@mail.ru>
2024-09-09 23:12:22 +0300

    задание 4

--- log_lab1/log_lab1.cpp
+++ log_lab1/log_lab1.cpp
@@ -5,24 +5,41 @@ int main()
 {
 	srand(time(0));
 	setlocale(LC_ALL, "");
-	int size=0, max = INT_MIN, min = INT_MAX, razn;
-	printf("Введите размер массива:");
-	scanf("%d", &size);
-
-	int* mass;
-	mass = (int*)malloc(size * sizeof(int));
+	int stroca,stolb, max = INT_MIN, min = INT_MAX, razn;
+	printf("Введите кол-во строк:");
+	scanf("%d", &stroca);
+	printf("Введите кол-во столбцов:");
+	scanf("%d", &stolb);
+	
+	int **mass = (int**)malloc(stroca*sizeof(int*));
+	for (int i = 0; i < stroca; i++) {
+		mass[i] = (int*)malloc(stolb * sizeof(int));
+	}
 
 	printf("Массив:");
-	for (int i = 0; i < size; i++) {
-		mass[i] = rand() % 100;
-		printf("%3d", mass[i]);
-		if (mass[i] >= max) {
-			max = mass[i];
-		}
-		if (mass[i] <= min) {
-			min = mass[i];
+	for (int i = 0; i < stroca; i++) {
+		printf("\n");
+		for (int j = 0; j < stolb; j++) {
+			mass[i][j] = rand() % 100;
+			printf("%3d", mass[i][j]);
+			if (mass[i][j] >= max) {
+				max = mass[i][j];
+			}
+			if (mass[i][j] <= min) {
+				min = mass[i][j];
+			}
 		}
 	}
+
+	printf("\nСумма по столбцам:\n");
+	for (int j = 0; j < stolb; j++) {
+		int sum = 0;
+		for (int i = 0; i < stroca; i++) {
+			sum += mass[i][j];
+		}
+		printf("Столбец %d: %d\n", j + 1, sum);
+	}
+		
 	razn = max - min;
 	printf("\nМаксимальный элемент:%d\nМинимальный элемент:%d\nРазница:%d", max, min, razn);
 
